e54870174fad368c36f345efd465054fc8861f1c
zorrilloQl <ro.canchaya@duocuc.cl>
2025-09-09 01:51:10 -0300

    ya ni se que hice

--- registro.js
+++ registro.js
@@ -3,14 +3,15 @@ const email = document.getElementById('email')
 const password = document.getElementById('password')
 const form = document.getElementById('form')
 const parrafo = document.getElementById('warnings')
+const edad = document.getElementById('age')
 
 
 form.addEventListener('submit', e=>{
     e.preventDefault()
     let warnings = ''
     let entrar = false
-    let regexEmail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.\w{2,3})+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
-    let emailduoc = /^[a-zA-Z0-9._%+-]+@duocuc\.cl$/
+    let regexEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
+    let emailduoc = /^[a-zA-Z0-9._%+-]+@duocuc\.cl?/g
     parrafo.innerHTML = ''
     if(nombre.value.length > 20){
         warnings += 'Maximo 20 caracteres <br>'
@@ -19,11 +20,13 @@ form.addEventListener('submit', e=>{
     if(!regexEmail.test(email.value)){
         warnings += 'El correo electronico no es valido <br>'
         entrar = true
-    }else{
-    if(!emailduoc.test(email.value)){
+    }
+    
+    if(emailduoc.test(email.value)){
         warnings += '¡Como eres parte de la comunidad Duoc UC, recuerda canjear tu torta gratis el día de tu cumpleaños!'
         entrar = true
-    }}
+    }
+    
     if(password.value.length < 8){
         warnings += 'Minimo de 8 caracteres <br>'
         entrar = true
